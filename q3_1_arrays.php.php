6fd34d2794d9eac7b7d63a27e6530039c2780b75
Yameeni <yameenimhaske1@gmail.com>
2020-09-30 20:25:02 +0530

    day1

---
+++ q3_1_arrays.php.php
@@ -0,0 +1,5 @@
+<?php
+$days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+echo "The days of week are: <br>" . $days[0] . "<br> " . $days[1] . " <br> " . $days[2] . "<br>" .$days[3]. 
+"<br>" .$days[4]. "<br> " . $days[5]. "<br> " . $days[6]. "<br>";
+?>
