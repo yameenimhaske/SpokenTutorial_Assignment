711713ac1b9555ad05f64e8f3f3667d0549f0909
yameenimhaske <68456311+yameenimhaske@users.noreply.github.com>
2020-10-01 12:03:38 +0530

    Delete q2_1_operators.php.php

--- q2_1_operators.php.php
+++
@@ -1,9 +0,0 @@
-<?php
-$x=40;
-$y=80;
-echo "The sum of x and y is : ". ($x+$y) ."<br />";
-echo "The difference between x and y is : ". ($x-$y) ."<br />";
-echo "Multiplication of x and y : ". ($x*$y) ."<br />";
-echo "Division of x and y : ". ($x/$y) ."<br />";
-echo "Modulus of x and y : " . ($x%$y) ."<br />";
-?>
